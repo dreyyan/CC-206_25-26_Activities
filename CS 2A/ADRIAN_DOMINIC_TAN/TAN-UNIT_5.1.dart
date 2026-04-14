328ceaf181f713642b3aface005fb4c8863d94bf
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-04-14 18:25:09 +0800

    add: .dart file for Unit 5.1 activity

---
+++ CS 2A/ADRIAN_DOMINIC_TAN/TAN-UNIT_5.1.dart
@@ -0,0 +1,131 @@
+// [CONCEPT] ENUM (Custom Data Type)
+enum AnimalKingdom {
+  MAMMAL,
+  BIRD,
+  REPTILE,
+  AMPHIBIAN,
+  FISH,
+  INSECT,
+  ARACHNID,
+  MOLLUSK,
+  CRUSTACEAN,
+  OTHER
+}
+
+// [CONCEPT] CLASS + CONSTRUCTOR
+class Animal {
+  final String Name;
+  final AnimalKingdom Kingdom;
+  final DateTime Dob;
+  final int Numlegs;
+
+  Animal(this.Name, this.Kingdom, this.Dob, this.Numlegs);
+
+  // [CONCEPT] FAT ARROW FUNCTION
+  void Walk(String direction) =>
+      print("The $Name is walking to the $direction.");
+
+  String displayInfo() {
+    // [CONCEPT] MULTILINE STRING
+    return """
+      Name: $Name
+      Kingdom: $Kingdom
+      Date of Birth: ${Dob.toLocal()}
+      Number of Legs: $Numlegs
+      """;
+  }
+}
+
+// [CONCEPT] CLASS + NAMED CONSTRUCTOR
+class Pet {
+  String? Nickname;
+  int Kindness = 0;
+
+  // Default constructor
+  Pet();
+
+  // [CONCEPT] NAMED CONSTRUCTOR
+  Pet.withNickname(this.Nickname) {
+    Kindness = 100;
+  }
+
+  void Kick(int amount) {
+    Kindness -= amount;
+    print("$Nickname's kindness decreased by $amount. Current kindness: $Kindness");
+  }
+
+  void pet(int amount) {
+    // [CONCEPT] CONDITIONAL
+    if (Kindness < 0) {
+      print("Failed to pet $Nickname. Kindness is negative.");
+    } else {
+      Kindness += amount;
+      print("$Nickname's kindness increased by $amount. Current kindness: $Kindness");
+    }
+  }
+
+  void feed(int numOfTreats) {
+    Kindness += numOfTreats * 2;
+    print("$Nickname has been fed with $numOfTreats treats. Current kindness: $Kindness");
+  }
+}
+
+// [CONCEPT] ENTRY POINT (main)
+void main() {
+  // [CONCEPT] LIST + TYPE INFERENCE
+  var ZOO1 = [
+    Animal("Lion", AnimalKingdom.MAMMAL, DateTime(2018, 5, 10), 4),
+    Animal("Eagle", AnimalKingdom.BIRD, DateTime(2020, 3, 15), 2),
+    Animal("Snake", AnimalKingdom.REPTILE, DateTime(2019, 7, 20), 0),
+    Animal("Frog", AnimalKingdom.AMPHIBIAN, DateTime(2021, 1, 5), 4),
+    Animal("Shark", AnimalKingdom.FISH, DateTime(2017, 9, 12), 0),
+  ];
+
+  print("-=-=-=- Zoo -=-=-=-");
+
+  // [CONCEPT] LOOP (FOR-IN)
+  for (var animal in ZOO1) {
+    print(animal.displayInfo());
+    animal.Walk("north");
+    print("-------------------");
+  }
+
+  // [CONCEPT] LIST + OBJECT CREATION (no 'new')
+  var PET_HOME = [
+    Pet.withNickname("Buddy"),
+    Pet.withNickname("Milo"),
+    Pet(),
+  ];
+
+  print("\n-=-=-=- Pet Home -=-=-=-");
+
+  // Make pets negative
+  PET_HOME[0].Kick(200);
+  PET_HOME[2].Kick(50);
+
+  // Make pets above 1000
+  PET_HOME[1].feed(600);
+  PET_HOME[1].pet(100);
+
+  print("\n-=-=-=- Pet Home Details -=-=-=-");
+
+  for (var pet in PET_HOME) {
+    print("Nickname: ${pet.Nickname}, Kindness: ${pet.Kindness}");
+  }
+
+  // [CONCEPT] MAP (Dictionary)
+  Map<String, int> petSummary = {
+    "Total Pets": PET_HOME.length,
+    "First Pet Kindness": PET_HOME[0].Kindness,
+  };
+
+  print("\nPet Summary: $petSummary");
+
+  // [CONCEPT] SPREAD OPERATOR
+  var extraAnimals = [
+    Animal("Dog", AnimalKingdom.MAMMAL, DateTime(2022, 1, 1), 4)
+  ];
+
+  var ALL_ANIMALS = [...ZOO1, ...extraAnimals];
+  print("\nTotal animals after spread: ${ALL_ANIMALS.length}");
+}
